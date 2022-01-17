2a1f007520c0e4b78aec55d692d8a0d060cebefb
Melis Nur Verir <melisnurverir@gmail.com>
2022-01-17 19:52:32 +0300

    adds data types

---
+++ data_type.R
@@ -0,0 +1,50 @@
+# integer
+# assignment operator (<-)
+x <-  2L
+typeof(x)
+
+# double
+y <- 2.5
+typeof(y)
+
+# complex
+z <- 3 + 2i
+typeof(z)
+
+# character
+a <- "m"
+typeof(a)
+
+# logical
+q <- T
+typeof(q)
+q2 <- FALSE
+
+
+# USING VARIABLES
+A <- 10
+B <- 5
+
+# Assign these variables to C
+C <- A + B
+C
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1 / var2
+result
+
+# variables used in functions as arguments
+answer <- sqrt(var2)
+answer
+
+
+# joinin strings 
+greeting <- "Hello"
+name <- "Melis"
+message <- paste(greeting,name)
+message
+
+
+
