4223e8b7071fdb5f3c3d5973af12966b554c16d8
Melis Nur Verir <melisnurverir@gmail.com>
2022-01-17 20:05:25 +0300

    adds logical variables and operators

---
+++ Core Programming Principles/logical_variables_operators.R
@@ -0,0 +1,17 @@
+# Logical
+# TRUE (T) or FALSE (F)
+
+# isTrue(x)
+
+result <- 4 < 5
+result
+
+result2 <- !TRUE
+result2
+
+result3 <- !(5 > 1)
+result3
+
+result | result3 # TRUE
+result & result2 # FALSE
+
