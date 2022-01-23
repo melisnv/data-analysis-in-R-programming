01dc62b3b0a351776d668f003667576c98f9f83c
Melis Nur Verir <melisnurverir@gmail.com>
2022-01-23 12:25:18 +0300

    adds exercise

---
+++ Core Programming Principles/law_of_large_numbers.R
@@ -0,0 +1,21 @@
+
+#Test the Law of Large Numbers for N random normally distributed
+# numbers with mean = 0, stdev =1.
+
+# specify sample size
+N <- 1000
+count <- 0 # reset counter
+
+# count how many of the numbers are between -1 and 1
+for (i in rnorm(N)) {
+  if(i >= -1 & i <= 1){
+    count <- count + 1
+  }
+}
+
+# calculate hit ratio
+mean <- count / N*100
+
+# check if mean > e(x)
+
+mean
